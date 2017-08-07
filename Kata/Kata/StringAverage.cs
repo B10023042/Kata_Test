089fd5944a8e8088b1fa35bbb8ecd15531bc23e6
cloud.chen <cloud.chen@titansoft.com.sg>
2017-08-07 10:08:02 +0800

    add project

---
+++ Kata/Kata/StringAverage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class StringAverage
+    {
+        static void Main(string[] args)
+        {
+        }
+        static string[] TranslastionList = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        public static string AverageString(string input)
+        {
+            var inputFormatError = -999;
+            var result = Sum(input.Split());
+            return (result != inputFormatError) ? Average(result, input) : "n/a";
+
+        }
+        private static string Average(int result, string input)
+        {
+            try
+            {
+                return TranslastionList[result / input.Split().Length];
+            }
+            catch
+            {
+                return "n/a";
+            }
+        }
+
+        public static int Sum(String[] input)
+        {
+            var sumNum = 0;
+            foreach (var i in input)
+            {
+                if (Array.IndexOf(TranslastionList, i) == -1) return -999;
+                sumNum += Array.IndexOf(TranslastionList, i);
+            }
+            return sumNum;
+        }
+    }
+}
---
+++ Kata/KataTestCase/StringAverageUnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+namespace KataTestCase
+{
+    [TestClass]
+    public class StringAverageUnitTest1
+    {
+        [TestMethod]
+        public void NullInputTest()
+        {
+            StringAverageTestCase("n/a", "");
+        }
+        [TestMethod]
+        public void OneInputTest()
+        {
+            StringAverageTestCase("four", "four");
+        }
+        [TestMethod]
+        public void InputZeroTest()
+        {
+            StringAverageTestCase("zero", "zero");
+        }
+        [TestMethod]
+        public void TwoInputTest()
+        {
+            StringAverageTestCase("four", "five four");
+        }
+        [TestMethod]
+        public void FourInputTest()
+        {
+            // StringAverageTestCase("four", "zero nine five two");
+            StringAverageTestCase("n/a", "123 six two three");
+            //StringAverageTestCase("two", "one one eight one");
+        }
+        [TestMethod]
+        public void SixInputTest()
+        {
+            StringAverageTestCase("three", "one two three four five");
+        }
+        private void StringAverageTestCase(string expect, string input)
+        {
+            var actual = StringAverage.AverageString(input);
+            Assert.AreEqual(actual, expect);
+        }
+    }
+}
