f82315b459097fac87176882bf52b98b790c6fdb
cloud.chen <cloud.chen@titansoft.com.sg>
2017-08-07 10:15:49 +0800

    add DeadAnts project

---
+++ Kata/Kata/DeadAnts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class DeadAnts
+    {
+
+        public static int DeadAntCount(string input)
+        {
+            try
+            {
+                input = input.Replace("ant", "");
+                var head = input.Count(x => x == 'a');
+                var body = input.Count(x => x == 'n');
+                var end = input.Count(x => x == 't');
+                return getMaxDeadAnt(head, body, end);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private static int getMaxDeadAnt(int head, int body, int end)
+        {
+            var maxDeadAnt = Math.Max(head, body);
+            maxDeadAnt = Math.Max(maxDeadAnt, end);
+            return maxDeadAnt;
+        }
+    }
+}
---
+++ Kata/KataTestCase/DeadAntsUnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+namespace KataTestCase
+{
+    [TestClass]
+    public class DeadAntsUnitTest1
+    {
+        [TestMethod]
+        public void emptyInputShouldReturnZero()
+        {
+            deadAntsTestCase(0, "");
+        }
+        [TestMethod]
+        public void nullInputShouldReturnZero()
+        {
+            deadAntsTestCase(0, null);
+        }
+        [TestMethod]
+        public void foutAntsInputShouldReturnZero()
+        {
+            deadAntsTestCase(0, "ant ant ant ant");
+        }
+        [TestMethod]
+        public void oneDeadAntsInputShouldReturnOne()
+        {
+            deadAntsTestCase(1, "ant ant .... a nt");
+        }
+        [TestMethod]
+        public void twoDeadAntsInputShouldReturnTwo()
+        {
+            deadAntsTestCase(2, "ant anantt aantnt");
+        }
+
+        protected void deadAntsTestCase(int expect, string input)
+        {
+            var actual = DeadAnts.DeadAntCount(input);
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
