8380aedfb471e67c19235e2801155480b5ec11ff
cloud.chen <cloud.chen@titansoft.com.sg>
2017-08-10 16:24:01 +0800

    add Band name generator

---
+++ Kata/Kata/BandNameGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class BandNameGenerator
+    {
+
+        public static string getNewName(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+            {
+                return "";
+            }
+
+            var firstCharUpperString = getFirstCharUpperString(input);
+
+            if (checkFormat(input))
+            {
+                return getAddTheString(firstCharUpperString);
+            }
+            else
+            {
+                return getCombinedString(input, firstCharUpperString);
+            }
+         
+        }
+
+        private static string getCombinedString(string input, string firstCharUpperString)
+        {
+            var namePart = input.Substring(1);
+            return firstCharUpperString + namePart;
+        }
+
+        private static string getAddTheString(string firstCharUpperString)
+        {
+            var namePart = "The ";
+            return namePart + firstCharUpperString;
+        }
+
+        private static bool checkFormat(string input)
+        {
+            return input[0] != input[input.Length - 1];
+        }
+
+        private static string getFirstCharUpperString(string input)
+        {
+            return input.Substring(0, 1).ToUpper()+input.Substring(1);
+        }
+    }
+}
---
+++ Kata/KataTestCase/BandNameGeneratorUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+namespace KataTestCase
+{
+    [TestClass]
+    public class BandNameGeneratorUnitTest
+    {
+        [TestMethod]
+        public void inputEmptyShouldBeEmpty()
+        {
+            var input = "";
+            var expect = "";
+            BandNameGeneratorTestCase(input,expect);
+        }
+        [TestMethod]
+        public void input_knife_ShouldBeThe_Knife()
+        {
+            var input = "knife";
+            var expect = "The Knife";
+            BandNameGeneratorTestCase(input, expect);
+        }
+        [TestMethod]
+        public void input_tart_ShouldBeTartart()
+        {
+            var input = "tart";
+            var expect = "Tartart";
+            BandNameGeneratorTestCase(input, expect);
+        }
+
+        private static void BandNameGeneratorTestCase(string input,string expect)
+        {
+            var actual = BandNameGenerator.getNewName(input);
+            Assert.AreEqual(actual, expect);
+        }
+    }
+}
