39179577c5e31323d115257cfc727ea073e34998
cloud.chen <cloud.chen@titansoft.com.sg>
2017-08-11 18:02:47 +0800

    add Mumbling

---
+++ Kata/Kata/Mumbling.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class Mumbling
+    {
+        public static string Accum(string input)
+        {
+            if(string.IsNullOrEmpty(input)) return String.Empty;
+
+            if (input.Length == 1)
+            {
+                return input.ToUpper();
+            }
+            else
+            {
+                input = input.ToLower();
+                var firstChar = char.ToUpper(input[0]).ToString();
+                for (var i =1; i < input.Length; i++)
+                {
+                    var upperString = char.ToUpper(input[i]);
+                    var lowerString = new string( input[i],i );
+                    var partString = upperString + lowerString;
+                    firstChar = firstChar + "-" + partString;
+                }
+
+                return firstChar;
+            }
+            
+         
+        }
+    }
+}
---
+++ Kata/KataTestCase/MumblingUnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+namespace KataTestCase
+{
+    [TestClass]
+    public class MumblingUnitTest1
+    {
+        [TestMethod]
+        public void inputEmptyShouldReturnEmpty()
+        {
+            string input=string.Empty;
+            string expect = string.Empty;
+            MumblingTestCase(input,expect);
+        }
+
+        [TestMethod]
+        public void inputOneLowerCharShouldReturnUpperChar()
+        {
+            string input = "a";
+            string expect = "A";
+            MumblingTestCase(input, expect);
+        }
+
+        [TestMethod]
+        public void inputOneUpperCharShouldReturnUpperChar()
+        {
+            string input = "B";
+            string expect = "B";
+            MumblingTestCase(input, expect);
+        }
+
+        [TestMethod]
+        public void inputTwoCharShouldReturnRightFormat()
+        {
+            string input = "Cd";
+            string expect = "C-Dd";
+            MumblingTestCase(input, expect);
+        }
+
+        [TestMethod]
+        public void inputThreeCharShouldReturnRightFormat()
+        {
+            string input = "aCd";
+            string expect = "A-Cc-Ddd";
+            MumblingTestCase(input, expect);
+        }
+
+        private static void MumblingTestCase(string input, string expect)
+        {
+            var actual = Mumbling.Accum(input);
+            Assert.AreEqual( expect,actual);
+        }
+    }
+}
