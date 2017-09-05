337695834cb7ddf41aea604171cb650e01bab263
cloud.chen <cloud.chen@titansoft.com.sg>
2017-09-05 18:15:52 +0800

    1.refactor DeadAnts 2.add TwoSum.cs

--- Kata/Kata/DeadAnts.cs
+++ Kata/Kata/DeadAnts.cs
@@ -11,22 +11,18 @@ namespace Kata
 
         public static int DeadAntCount(string input)
         {
-            try
-            {
-                input = input.Replace("ant", "");
-                var head = input.Count(x => x == 'a');
-                var body = input.Count(x => x == 'n');
-                var end = input.Count(x => x == 't');
-                return getMaxDeadAnt(head, body, end);
-            }
-            catch
-            {
-                return 0;
-            }
+            if (String.IsNullOrEmpty(input)) return 0;
+            input = input.Replace("ant", string.Empty);
+            var head = input.Count(x => x == 'a');
+            var body = input.Count(x => x == 'n');
+            var end = input.Count(x => x == 't');
+            return getMaxDeadAnt(head, body, end);
         }
+           
 
         private static int getMaxDeadAnt(int head, int body, int end)
         {
+            if (head==0 && body ==0 && end==0) return 0;
             var maxDeadAnt = Math.Max(head, body);
             maxDeadAnt = Math.Max(maxDeadAnt, end);
             return maxDeadAnt;
