c6dbf281db184cbf9e5cb189b1fb8eec81f22ae1
cloud.chen <cloud.chen@titansoft.com.sg>
2017-09-05 18:17:14 +0800

    add TwoSum

---
+++ Kata/Kata/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class TwoSum
+    {
+        public static int[] calTwoSum(int[] nums, int target)
+        {
+            foreach (var num in nums)
+            {
+                var reduceNum = target - num;
+                var numIndex =  Array.IndexOf(nums, num);
+                if (Array.IndexOf(nums, reduceNum, numIndex+1) >= 0)
+                {
+                    int[] answerList = new int[2];
+                    answerList[0] = numIndex;
+                    answerList[1] = Array.IndexOf(nums, reduceNum);
+                    return answerList;
+                }
+            }
+            return new int [] {0,0};
+        }
+    }
+}
---
+++ Kata/KataTestCase/TwoSumUnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+namespace KataTestCase
+{
+    [TestClass]
+    public class TwoSumUnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var targetNum = 9;
+            var target = new int[] { 0, 1 };
+            var actual = TwoSum.calTwoSum(nums,targetNum);
+            CollectionAssert.AreEqual(target, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var targetNum = 18;
+            var target = new int[] { 1, 2 };
+            var actual = TwoSum.calTwoSum(nums, targetNum);
+            CollectionAssert.AreEqual(target, actual);
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var nums = new int[] {3,2,4};
+            var targetNum = 6;
+            var target = new int[] { 1, 2 };
+            var actual = TwoSum.calTwoSum(nums, targetNum);
+            CollectionAssert.AreEqual(target, actual);
+        }
+    }
+}
