b9ffa0e3d1e16a1571cc994cda83354be70baaee
cloud.chen <cloud.chen@titansoft.com.sg>
2017-09-21 09:27:44 +0800

    add ContainsDuplicateClass

---
+++ Kata/Kata/ContainsDuplicateClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class ContainsDuplicateClass
+    {
+        public static bool containsDuplicate(int[] nums, int k)
+        {
+            if (isInputWorngFormat(nums, k)) return false;   
+        
+            var maxValue = nums.Length/k;
+            for (var i = 0; i <= maxValue; i++)
+            {
+                var firstValue = nums[i];
+                for (var j = 1; j <= k; j++)
+                {
+                    var secondValueIndex = i + j;
+                    if (secondValueIndex < nums.Length)
+                    {
+                        var secondValue = nums[secondValueIndex];
+                        if (firstValue == secondValue) return true;
+                    }                    
+                }        
+            }
+                return false;
+        }
+        private static bool isInputWorngFormat(int[] nums, int k)
+        {
+            if (nums.Length <= 1) return true;
+            if (k == 0) return true;
+            return false;
+        }
+    }
+}
---
+++ Kata/KataTestCase/ContainsDuplicateUnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+namespace KataTestCase
+{
+    [TestClass]
+    public class ContainsDuplicateUnitTest1
+    {
+        [TestMethod]
+        public void inputEmpty_and0_should_return_false()
+        {
+            ContainsDuplicateUnitTestClass(
+                new int[] {},
+                0,
+                false);
+        }
+
+        [TestMethod]
+        public void input1_and0_should_return_false()
+        {
+            ContainsDuplicateUnitTestClass(
+                new int[] {1},
+                0,
+                false);
+        }
+
+        [TestMethod]
+        public void input2_2_and3_should_return_true()
+        {
+            ContainsDuplicateUnitTestClass(
+                new int[] { 2,2 },
+                3,
+                true);
+        }
+
+        [TestMethod]
+        public void input1_2_1_and0_should_return_false()
+        {
+            ContainsDuplicateUnitTestClass(
+                new int[] { 1,2,1 },
+                0,
+                false);
+        }
+
+
+        [TestMethod]
+        public void input2_3_4_5_3_1_and3_should_return_true()
+        {
+            ContainsDuplicateUnitTestClass(
+                new int[] { 2, 3, 4, 5, 3, 1 },
+                3,
+                true);
+        }
+
+
+        [TestMethod]
+        public void input2_3_4_5_3_1_and2_should_return_false()
+        {
+            ContainsDuplicateUnitTestClass(
+                new int[] { 2, 3, 4, 5, 3, 1 },
+                2,
+                false);
+        }
+
+
+        [TestMethod]
+        public void input99_99_and2_should_return_true()
+        {
+            ContainsDuplicateUnitTestClass(
+                new int[] {99,99},
+                2,
+                true);
+        }
+
+
+        private static void ContainsDuplicateUnitTestClass(int[] inputNums,int k, bool expect)
+        {            
+            var actual = ContainsDuplicateClass.containsDuplicate(inputNums, k);
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
