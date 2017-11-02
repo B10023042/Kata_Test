adbb912b10326a6326f332b63e0a7b9443438e9c
Cloud <thirmoon@gmail.com>
2017-11-02 23:17:57 +0800

    add ContainsDuplicateIII

---
+++ Kata/Kata/ContainsDuplicateIII.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Kata
+{
+    public class ContainsDuplicateIII
+    {
+        public static bool ContainsDuplicate(int[] nums, int k, int t)
+        {
+            if (IsInputWorngFormat(nums, k, t)) return false;
+
+            for (var i = 0; i < nums.Length-1; i++)
+            {
+                var firstValue = nums[i];
+
+                if (t == 0)
+                {
+                    return IsHasDuplicate(nums);
+                }
+                var searchRange = nums.Skip(i + 1).Take(k);
+               
+                if  (searchRange.
+                    Any(x => DifferenceValue(firstValue, x) <= t))
+                    {
+                        return true;
+                    }
+               }
+            return false;
+        }
+
+        private static long DifferenceValue(int firstValue, int x)
+        {
+            return Math.Abs(((long)firstValue - (long)x));
+        }
+
+        private static bool IsHasDuplicate(int[] nums)
+        {
+            return nums
+                .GroupBy(item => item)
+                .Select(item => item.Count())
+                .Any(item => item > 1);
+        }
+
+        private static bool IsInputWorngFormat(int[] nums, int k, int t)
+        {
+            return nums.Length <= 1;
+        }
+
+    }
+}
---
+++ Kata/KataTestCase/ContainsDuplicateIIIUnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Kata;
+using NUnit.Framework;
+
+namespace KataTestCase
+{
+    [TestFixture]
+    public class ContainsDuplicateIIIUnitTest1
+    {
+
+        [TestCase(new int[0], 0, 0, false)]
+        public void TestEmptyArray(int[] nums, int k, int t, bool expected)
+        {
+            ContainsDuplicateIIIUnitTestCase(nums, k, t, expected);
+        }
+        [TestCase(new[] { -1, -1 }, 1, 0, true)]
+        [TestCase(new[] { 1, 3, 1 }, 2, 1, true)]
+        [TestCase(new[] { 1, 3, 1 }, 1, 1, false)]
+        [TestCase(new[] { 1, 2 }, 0, 1, false)]
+        [TestCase(new[] { 7, 1, 3 }, 2, 3, true)]
+        [TestCase(new[] { 4,2}, 2, 1, false)]
+        [TestCase(new[] { -3,3}, 2, 4, false)]
+        [TestCase(new[] { 4, 1, -1, 6, 5 }, 3, 1, true)]
+        [TestCase(new[] { -1,-1 }, 1, -1, false)]
+        public void Input2NumArray(int[] nums, int k, int t, bool expected)
+        {
+            ContainsDuplicateIIIUnitTestCase(nums, k, t, expected);
+        }
+
+
+        [TestCase(new[] { -1, 2147483647 }, 1, 2147483647, false)]
+        public void specialCase(int[] nums, int k, int t, bool expected)
+        {
+            ContainsDuplicateIIIUnitTestCase(nums, k, t, expected);
+        }
+        private void ContainsDuplicateIIIUnitTestCase(int[] nums, int k,int t, bool expect)
+        {
+            var actual = ContainsDuplicateIII.ContainsDuplicate(nums,k,t);
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
