7b2ebff7cb633e430280e3301afd047da2530200
Cloud <thirmoon@gmail.com>
2017-11-04 21:42:47 +0800

    add TennisClass1

---
+++ Kata/Kata/TennisClass1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class TennisClass1
+    {
+        private static Player _playerA;
+        private static Player _playerB;
+        enum PointsDescribed
+        {
+            Love = 0, Fifteen, Thirty, Forty
+        }
+        public static void NewGame()
+        {
+            _playerA = new Player("PlayerA");
+            _playerB = new Player("PlayerB");
+        }
+
+        public static void PlayerWinBall(string playerName)
+        {
+            if (HasAdvantage()) CancelAdvantage();
+            if (IsDeuce()) GetPlayer(playerName).GetAdvantage();
+            GetPlayer(playerName).WinTheBall();
+        }
+
+        private static void CancelAdvantage()
+        {
+            if (_playerA.IsAdvantage) _playerA.IsAdvantage = false;
+            if (_playerB.IsAdvantage) _playerB.IsAdvantage = false;
+        }
+
+        private static Player GetPlayer(string playerName)
+        {
+            return playerName.Equals("PlayerA") ? _playerA : _playerB;
+        }
+
+        private static bool IsDeuce()
+        {
+            return _playerA.Score >= (int)PointsDescribed.Forty && _playerB.Score == _playerA.Score;
+        }
+
+        public static string GetResult()
+        {
+            if (IsDeuce()) return "Deuce";
+            if (HasAdvantage()) return $"{ Winner().PlaylerName} Advantage";
+            if (HasWinner()) return $"{ Winner().PlaylerName} Win";
+            return $"{ (PointsDescribed)Winner().Score} {(PointsDescribed)Loser().Score}";
+        }
+
+        private static Player Loser()
+        {
+            return _playerA.Score < _playerB.Score ? _playerA : _playerB;
+        }
+
+        private static Player Winner()
+        {
+            return _playerA.Score > _playerB.Score ? _playerA : _playerB;
+        }
+        private static bool HasWinner()
+        {
+            return (Winner().Score > (int)PointsDescribed.Forty);
+        }
+
+        private static bool HasAdvantage()
+        {
+            return _playerA.IsAdvantage || _playerB.IsAdvantage;
+        }
+
+        private class Player
+        {
+            public string PlaylerName { get; set; }
+            public int Score { get; private set; }
+            public bool IsAdvantage { get; set; }
+            public Player(string playerName)
+            {
+                this.PlaylerName = playerName;
+                this.Score = 0;
+                this.IsAdvantage = false;
+            }
+            internal void WinTheBall()
+            {
+                this.Score = this.Score + 1;
+            }
+
+            internal void GetAdvantage()
+            {
+                this.IsAdvantage = true;
+            }
+        }
+    }
+}
---
+++ Kata/KataTestCase/TennisUnitTest1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+namespace KataTestCase
+{
+    [TestClass]
+    public class TennisUnitTest1
+    {
+        [TestMethod]
+        public void newGame_Return_Love_And_Love()
+        {
+            TennisClass1.NewGame();
+
+            var expect = "Love Love";
+            var actual = TennisClass1.GetResult();
+            Assert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void PlayerA_WinBall_Return_Fifteen_And_Love()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerA");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "Fifteen Love";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void PlayerB_WinBall_Return_Fifteen_And_Love()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "Fifteen Love";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Both_WinBall_Return_Fifteen_And_Fifteen()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "Fifteen Fifteen";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void PlayerA_WinBall_Twice_And_PlayerB_WinBall_Once_Return_Thirty_And_Fifteen()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "Thirty Fifteen";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void PlayerA_WinBall_3times_And_PlayerB_WinBall_Once_Return_Forty_And_Fifteen()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "Forty Fifteen";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void PlayerA_WinBall_4times_And_PlayerB_WinBall_Once_Return_PlayerAWin()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "PlayerA Win";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void PlayerB_WinBall_4times_And_PlayerA_WinBall_Once_Return_PlayerBWin()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "PlayerB Win";
+            Assert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void Both_WinBall_3times_Return_Deuce()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "Deuce";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void AfterDeuce_PlayerAWinBall_Return_PlayerA_Advantage()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            var actual = TennisClass1.GetResult();
+            var expect = "PlayerA Advantage";
+            Assert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void AfterDeuce_PlayerBWinBall_Return_PlayerB_Advantage()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            var actual = TennisClass1.GetResult();
+            var expect = "PlayerB Advantage";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void AfterDeuce_BothWinBall_Return_Deuce()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "Deuce";
+            Assert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void AfterDeuce_BothWinBall_Return_Deuce_2()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "Deuce";
+            Assert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void AfterTwiceDeuce_PlayerA_WinBalll_Return_PlayerA_Advantage()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "PlayerA Advantage";
+            Assert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void AfterTwiceDeuce_PlayerA_WinBalll_Return_PlayerB_Advantage()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "PlayerB Advantage";
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void AfterTwiceDeuce_Both_WinBalll_Return_Deuce()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "Deuce";
+            Assert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void AfterDeuce_PlayerA_WinBall_Twice_Return_PlayerAWin()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "PlayerA Win";
+            Assert.AreEqual(expect, actual);
+        }
+
+
+
+        [TestMethod]
+        public void AfterDeuce_PlayerB_WinBall_Twice_Return_PlayerBWin()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "PlayerB Win";
+            Assert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void AfterTwiceDeuce_PlayerB_WinBall_Twice_Return_PlayerBWin()
+        {
+            TennisClass1.NewGame();
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerA");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+            TennisClass1.PlayerWinBall("PlayerB");
+
+            var actual = TennisClass1.GetResult();
+            var expect = "PlayerB Win";
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
