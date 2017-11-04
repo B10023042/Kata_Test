a1051f3527ebb4b8bbd42373d08b35600e3e2fb8
Cloud <thirmoon@gmail.com>
2017-11-04 16:43:35 +0800

    add sumString

---
+++ Kata/Kata/SumStringsClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class SumStringsClass
+    {
+        public static string sumString(string inputA, string inputB)
+        {
+          
+            var numAarray = transformInputToIntArray(inputA);
+            var numBarray = transformInputToIntArray(inputB);
+          
+            var longArray = (numAarray.Length < numBarray.Length)? numBarray : numAarray;
+            var shortArray = (numAarray.Length < numBarray.Length) ? numAarray : numBarray;
+
+            var answer = "";
+            var carryNum = 0;
+            for (var i = 0; i < longArray.Length; i++)
+            {
+                if (i < shortArray.Length)
+                {
+                    var num = longArray[i] + shortArray[i] + carryNum;
+                    carryNum = 0;
+                    if (num > 9)
+                    {
+                        carryNum = 1;
+                        num = num - 10;
+                    }
+                    answer = string.Format("{0}{1}", num.ToString(), answer);
+                }
+                else if(longArray[i]!=0)
+                {
+                    answer = string.Format("{0}{1}", (longArray[i] + carryNum).ToString(), answer);
+                    carryNum = 0;
+                }
+            }
+            if(carryNum!=0)
+                answer = string.Format("{0}{1}", 1.ToString(), answer);
+            if (answer.Substring(0, 1) == "0") answer = answer.Substring(1, answer.Length - 1);
+            return answer;
+        }
+
+        private static int[] transformInputToIntArray(string input)
+        {
+            var intArray = input.ToCharArray().Select(c => Convert.ToInt32(c.ToString())).ToArray();
+            Array.Reverse(intArray);
+            return intArray;
+        }
+    }
+}
---
+++ Kata/KataTestCase/SumStringTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+namespace KataTestCase
+{
+    [TestClass]
+    public class SumStringTest1
+    {
+        [TestMethod]
+        public void Given1And2Returns3()
+        {
+            var expect = "3";
+            var actual = SumStringsClass.sumString("1","2");
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Given21And2Returns23()
+        {
+            var expect = "23";
+            var actual = SumStringsClass.sumString("21", "2");
+            Assert.AreEqual(expect, actual);
+        }
+       
+        [TestMethod]
+        public void Given21And24Returns45()
+        {
+            var expect = "45";
+            var actual = SumStringsClass.sumString("21", "24");
+            Assert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void GivenEmptyAnd5Returns5()
+        {
+            var expect = "5";
+            var actual = SumStringsClass.sumString("", "5");
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Given00103And08567Returns5()
+        {
+            var expect = "8670";
+            var actual = SumStringsClass.sumString("00000103", "08567");
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Given712569312664357328695151392And8100824045303269669937Returns712577413488402631964821329()
+        {
+            var expect = "712577413488402631964821329";
+            var actual = SumStringsClass.sumString("712569312664357328695151392", "8100824045303269669937");
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Given81055900096023504197206408605And0095301248058391139327916261Returns131151201344081895336534324866()
+        {
+            var expect = "131151201344081895336534324866";
+            var actual = SumStringsClass.sumString("81055900096023504197206408605", "50095301248058391139327916261");
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
