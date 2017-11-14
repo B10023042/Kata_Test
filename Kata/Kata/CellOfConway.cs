2f0bc918603f9be190dac8f4a975713f25ebf089
Cloud <thirmoon@gmail.com>
2017-11-14 23:25:59 +0800

    Add CellOfConway

---
+++ Kata/Kata/CellOfConway.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class CellOfConway
+    {
+        public StateOfCell CheckCheckState(StateOfCell state, int amoutOfAliveNeighbours)
+        {
+            if (state == StateOfCell.Dead)
+            {
+                return (amoutOfAliveNeighbours == 3) ? StateOfCell.Alive : StateOfCell.Dead;
+            }
+
+            return (amoutOfAliveNeighbours < 2) ? StateOfCell.Dead :
+                (amoutOfAliveNeighbours < 4) ? StateOfCell.Alive : StateOfCell.Dead;
+
+
+        }
+    }
+
+    public enum StateOfCell
+    {
+        Dead, Alive
+    }
+}
---
+++ Kata/KataTestCase/CellOfConwayUnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace KataTestCase
+{
+    [TestFixture]
+    public class CellOfConwayUnitTest1
+    {
+        [TestCase(0, StateOfCell.Dead)]
+        [TestCase(1, StateOfCell.Dead)]
+        [TestCase(2, StateOfCell.Alive)]
+        [TestCase(3, StateOfCell.Alive)]
+        [TestCase(4, StateOfCell.Dead)]
+        [TestCase(5, StateOfCell.Dead)]
+        [TestCase(6, StateOfCell.Dead)]
+        [TestCase(7, StateOfCell.Dead)]
+        [TestCase(8, StateOfCell.Dead)]
+        public void SelfIsAliveTest(int amoutOfAliveNeighbours, StateOfCell expectState)
+        {
+            var cellOfConway = new CellOfConway();
+            var expected = expectState;
+            var actual = cellOfConway.CheckCheckState(StateOfCell.Alive, amoutOfAliveNeighbours);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, StateOfCell.Dead)]
+        [TestCase(1, StateOfCell.Dead)]
+        [TestCase(2, StateOfCell.Dead)]
+        [TestCase(3, StateOfCell.Alive)]
+        [TestCase(4, StateOfCell.Dead)]
+        [TestCase(5, StateOfCell.Dead)]
+        [TestCase(6, StateOfCell.Dead)]
+        [TestCase(7, StateOfCell.Dead)]
+        [TestCase(8, StateOfCell.Dead)]
+        public void SelfIsDeadTest(int amoutOfAliveNeighbours, StateOfCell expectState)
+        {
+            var cellOfConway = new CellOfConway();
+            var expected = expectState;
+            var actual = cellOfConway.CheckCheckState(StateOfCell.Dead, amoutOfAliveNeighbours);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
