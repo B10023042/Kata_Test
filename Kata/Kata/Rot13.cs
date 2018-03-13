9839c565483ad17ffa6c0429e0718fb36381b6d6
Cloud <thirmoon@gmail.com>
2018-03-14 00:51:59 +0800

    refactor Rot13.cs

---
+++ Kata/Kata/Rot13.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class Rot13
+    {
+        public string Translate(string input)
+        {
+            var ans = "";
+            foreach (var s in input)
+            {
+                var asciiNum = Convert.ToInt32(s);
+                const int rotNum = 13;
+                if (IsFirstSectionAlphabet(asciiNum))
+                {
+                    asciiNum += rotNum;
+                }
+                else if (IsSecondSectionAlphabet(asciiNum))
+                {
+                    asciiNum -= rotNum;
+                }
+
+                var x = Convert.ToChar(asciiNum);
+                ans += x.ToString();
+            }
+            return ans;
+
+        }
+
+        private static bool IsSecondSectionAlphabet(int asciiNum)
+        {
+            return (asciiNum >= Convert.ToInt32('N') && asciiNum <= Convert.ToInt32('Z') || (asciiNum >= Convert.ToInt32('n') && asciiNum <= Convert.ToInt32('z')));
+        }
+
+        private static bool IsFirstSectionAlphabet(int asciiNum)
+        {
+            return (asciiNum >= Convert.ToInt32('A') && asciiNum <= Convert.ToInt32('M') || (asciiNum >= Convert.ToInt32('a') && asciiNum <= Convert.ToInt32('m')));
+
+        }
+    }
+}
---
+++ Kata/KataTestCase/Rot13Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace KataTestCase
+{
+    [TestFixture]
+    public class Rot13UnitTest1
+    {
+        [TestCase(" ", " ")]
+        [TestCase("oBB", "bOO")]
+        [TestCase("o", "b")]
+        [TestCase("ROT13 example.", "EBG13 rknzcyr.")]
+        public void TestMethod1(string input, string expectValue)
+        {
+            var rot13helper = new Rot13();
+            var expect = expectValue;
+            Assert.AreEqual(expect, rot13helper.Translate(input));
+        }
+    }
+}
